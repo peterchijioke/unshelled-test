424261dbde5dca4fb1269c3078a1f0d1f5835f4d
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-08 15:23:04 +0100

    Update routes.js

--- route/routes.js
+++ route/routes.js
@@ -7,15 +7,15 @@ import {
   orderItem,
   deletOrderItem,
 } from "../controller/orderController.js";
-import { login } from "../controller/userController.js";
+import { login, updateSellerDetails } from "../controller/userController.js";
 
 dotenv.config();
 connect();
-
 const route = express();
 route.use(express.json());
 route.use(express.urlencoded({ extended: true }));
 route.post("/login", login);
+route.post("/update-seller", auth, updateSellerDetails);
 route.get("/order_items", auth, orderItems);
 route.delete("/order_items/:id", auth, deletOrderItem);
 route.get("/order_item", auth, orderItem);
