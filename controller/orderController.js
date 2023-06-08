88c88073ac17e334a77e029d2a2bbeadc2c24899
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-08 21:18:57 +0100

    update

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,4 +1,5 @@
 import Order from "../model/order.js";
+import Product from "../model/product.js";
 
 // Get pagginated order items
 const orderItems = async (req, res) => {
@@ -6,11 +7,31 @@ const orderItems = async (req, res) => {
 
   try {
     const seller_id = req.headers.authorization.split(" ")[1];
-    const data = await Order.find({ seller_id })
+    const query = await Order.find({ seller_id })
       .sort({ price: -1 })
       .limit(limit * 1)
       .skip((page - 1) * limit)
       .exec();
+
+    const data = query.map(async (item) => {
+      const product = await Product.findOne({ product_id: item.product_id });
+      if (product.product_category_name) {
+        return {
+          id: item.order_item_id,
+          product_id: item.product_id,
+          product_category: product.product_category_name,
+          price: item.price,
+          date: item.shipping_limit_date,
+        };
+      }
+      return {
+        id: item.order_item_id,
+        product_id: item.product_id,
+        product_category: null,
+        price: item.price,
+        date: item.shipping_limit_date,
+      };
+    });
     return res.status(200).json({
       data,
       total: await Order.count(),
--- model/product.js
+++ model/product.js
@@ -3,12 +3,12 @@ import { Schema, model } from "mongoose";
 const ProductSchema = new Schema({
   product_id: { type: String, unique: true },
   product_category_name: { type: String, default: null },
-  product_name_lenght: { type: Number },
-  product_description_lenght: { type: Number },
-  product_photos_qty: { type: Number },
-  product_weight_g: { type: Number },
-  product_length_cm: { type: Number },
-  product_height_cm: { type: Number },
-  product_width_cm: { type: Number },
+  product_name_lenght: { type: Number, default: null },
+  product_description_lenght: { type: Number, default: null },
+  product_photos_qty: { type: Number, default: null },
+  product_weight_g: { type: Number, default: null, default: null },
+  product_length_cm: { type: Number, default: null, default: null },
+  product_height_cm: { type: Number, default: null },
+  product_width_cm: { type: Number, default: null },
 });
 export default model("olist_products_dataset", ProductSchema);
