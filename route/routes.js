3c36c77098f729e3c426c81910151fa4cb5b0ef6
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-08 10:06:26 +0100

    Update routes.js

--- route/routes.js
+++ route/routes.js
@@ -1,13 +1,11 @@
 // importing user context
 import dotenv from "dotenv";
 import { connect } from "../config/database.js";
-dotenv.config();
-connect();
 import express from "express";
-
 import auth from "../middleware/auth.js";
 import { home, login, register } from "../controller/userController.js";
-
+dotenv.config();
+connect();
 const route = express();
 route.use(express.json());
 route.post("/register", register);
